99792f6523370a610f47e76f4359ce8564324094
ntxtthomas <apm.tthomas@gmail.com>
2016-07-22 22:18:38 -0500

    navbar and grids

--- js/app.js
+++ js/app.js
@@ -2,12 +2,30 @@ $(document).ready(function(){
 
 	$("#dancer").click(function(){
 		$(".dance").slideToggle("slow");
-	});
+	})
 	$("#out").click(function(){
 		$(".horse").fadeOut("slow");
 	})
 	$("#in").click(function(){
 		$(".horse").fadeIn("slow");
 	})
+	$("#up").click(function(){
+		$(".up").slideUp("fast");
+		$(".down").slideUp("fast");
+	})
+	$("#down").click(function(){
+		$(".down").slideDown("fast");
+	})
+	$("#reset").click(function(){
+		$(".up").slideDown("fast");
+		$(".down").slideDown("fast");
+	})
+	$("#ham").click(function(){
+		$(".ham").removeClass();
+	})
+	$("#bacon").click(function(){
+		$(".bacon").addClass(".ham");
+	})
+	
 
 });
